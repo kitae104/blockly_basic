d1a4a207e12c73020b439b4df42e05b73cd6c0d6
Kitae's Github <aqua0405@gmail.com>
2024-11-10 00:33:16 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@
  */
 
 import * as Blockly from 'blockly';
-import {blocks} from './blocks/text';
-import {forBlock} from './generators/javascript';
-import {javascriptGenerator} from 'blockly/javascript';
-import {save, load} from './serialization';
-import {toolbox} from './toolbox';
+import { blocks } from './blocks/text';
+import { forBlock } from './generators/javascript';
+import { javascriptGenerator } from 'blockly/javascript';
+import { save, load } from './serialization';
+import { toolbox } from './toolbox';
 import './index.css';
 
 // Register the blocks and generator with Blockly
@@ -20,18 +20,39 @@ Object.assign(javascriptGenerator.forBlock, forBlock);
 const codeDiv = document.getElementById('generatedCode').firstChild;
 const outputDiv = document.getElementById('output');
 const blocklyDiv = document.getElementById('blocklyDiv');
-const ws = Blockly.inject(blocklyDiv, {toolbox});
+const ws = Blockly.inject(blocklyDiv, {
+    toolbox,
+    grid: { spacing: 20, length: 3, colour: '#ccc', snap: true },
+    move: {
+        scrollbars: {
+            horizontal: true,
+            vertical: true,
+        },
+        drag: true,
+        wheel: true,
+    },
+    zoom: {
+        controls: true,
+        wheel: true,
+        startScale: 1.0,
+        maxScale: 3,
+        minScale: 0.3,
+        scaleSpeed: 1.2,
+        pinch: true,
+    },
+    trashcan: true,
+});
 
 // This function resets the code and output divs, shows the
 // generated code from the workspace, and evals the code.
 // In a real application, you probably shouldn't use `eval`.
 const runCode = () => {
-  const code = javascriptGenerator.workspaceToCode(ws);
-  codeDiv.innerText = code;
+    const code = javascriptGenerator.workspaceToCode(ws);
+    codeDiv.innerText = code;
 
-  outputDiv.innerHTML = '';
+    outputDiv.innerHTML = '';
 
-  eval(code);
+    eval(code);
 };
 
 // Load the initial state from storage and run the code.
@@ -40,23 +61,23 @@ runCode();
 
 // Every time the workspace changes state, save the changes to storage.
 ws.addChangeListener((e) => {
-  // UI events are things like scrolling, zooming, etc.
-  // No need to save after one of these.
-  if (e.isUiEvent) return;
-  save(ws);
+    // UI events are things like scrolling, zooming, etc.
+    // No need to save after one of these.
+    if (e.isUiEvent) return;
+    save(ws);
 });
 
 // Whenever the workspace changes meaningfully, run the code again.
 ws.addChangeListener((e) => {
-  // Don't run the code when the workspace finishes loading; we're
-  // already running it once when the application starts.
-  // Don't run the code during drags; we might have invalid state.
-  if (
-    e.isUiEvent ||
-    e.type == Blockly.Events.FINISHED_LOADING ||
-    ws.isDragging()
-  ) {
-    return;
-  }
-  runCode();
+    // Don't run the code when the workspace finishes loading; we're
+    // already running it once when the application starts.
+    // Don't run the code during drags; we might have invalid state.
+    if (
+        e.isUiEvent ||
+        e.type == Blockly.Events.FINISHED_LOADING ||
+        ws.isDragging()
+    ) {
+        return;
+    }
+    runCode();
 });
